07553657b8a67342255cb1fda390931e365dff6c
besheir77 <bassammohamed398@gmail.com>
2025-05-22 22:51:13 +0300

    initial commit

--- src/modules/Shared/components/MasterLayout/MasterLayout.jsx
+++ src/modules/Shared/components/MasterLayout/MasterLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../SideBar/SideBar'
+import SideBar from '../SideBar/SideBar'
 import Navbar from '../Navbar/Navbar'
 
 export default function MasterLayout({userName}) {
@@ -8,7 +8,7 @@ export default function MasterLayout({userName}) {
     <>
       <div className='d-flex'>
         <div className="">
-          <Sidebar/>
+          <SideBar/>
         </div>
         <div className="w-100">
           <Navbar userName={userName}/>
